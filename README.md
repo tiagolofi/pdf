9b28571ef1411ecb6aa506ba9c8ab23e8feeddf3
tiagolofi <jejeco75@gmail.com>
2024-01-30 00:51:28 -0300

    readme, code and all more

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# pdf
+# Utils for PDF files
+
+## Installation
+
+```
+pip install pypdf
+```
+
+## How to Use
+
+```python
+from pdf import Pdf
+
+pdf = Pdf(file = 'File.pdf')
+
+print(pdf.__call__()) # metadata file
+```
+
+#### Juntar PDFs
+
+```python
+list_pdfs = Pdf(list_files = ['File 1.pdf', 'File 2.pdf'])
+
+list_pdfs.join(filename = 'New Name.pdf') # create new file called 'New Name.pdf' (filename parameter)
+```
+
+#### Reordenar/Excluir Páginas
+
+```python
+pdf = Pdf(file = 'File.pdf')
+
+pdf.reorder(pages_order=[3, 2, 4, 1]) # filename is default
+```
+
+#### Cortar
+
+_Soon_
---
+++ pdf.py
@@ -0,0 +1,57 @@
+
+from pypdf import PdfReader, PdfWriter
+from datetime import datetime
+
+class Pdf():
+    
+    def __init__(self, file: str = None, list_files: list = None) -> None:
+        '''
+        Attributes:
+            - file: .pdf file or path to file;
+            - list_files: .pdf files or paths to files.
+        '''
+        self.file = file
+        self.list_files = list_files
+        if file is not None:
+            self.length = len(PdfReader(self.file).pages) + 1
+
+    def __call__(self) -> dict:
+        if self.file is not None:
+            return PdfReader(self.file).metadata
+        else:
+            return {'Message': 'its a list of files, no metadata available.'}
+
+    def __timestamp(self) -> str:
+        '''return a default filename.'''
+        return 'file_' + str(int(datetime.now().timestamp())) + '.pdf'
+
+    def join(self, filename: str = None) -> None:
+        '''
+        Parameters:
+            - filename: new name for .pdf file, to default see: self.__timestamp.
+        '''
+        if filename is None:
+            filename = self.__timestamp()
+        pdf = PdfWriter()
+        for i in self.list_files:
+            pdf.append(i)
+        pdf.write(filename)
+        pdf.close()
+
+    def reorder(self, pages_order: list = None, filename: str = None) -> None:
+        '''
+        Parameters:
+            - pages_order: a list with the new order of the pages, ex: [4, 3, 1, 2];
+            - filename: new name for .pdf file, to default see: self.__timestamp.
+        '''
+        if filename is None:
+            filename = self.__timestamp()
+        if pages_order is None:
+            pages_order = range(1, self.length)
+        pdf_reader = PdfReader(self.file)
+        pdf_writer = PdfWriter()
+        for i in pages_order:
+            pdf_writer.add_page(pdf_reader.pages[i - 1])
+        pdf_writer.add_metadata(pdf_reader.metadata)
+        pdf_writer.write(filename)
+        pdf_writer.close()
